2486a4f40c2e5398ee8a62f2d6de6c37e7440c81
morimolymoly <morimolymoly@gmail.com>
2021-11-15 08:39:54 +0900

    first commit

---
+++ tests/gate.ts
@@ -0,0 +1,17 @@
+import * as anchor from '@project-serum/anchor';
+import { Program } from '@project-serum/anchor';
+import { Gate } from '../target/types/gate';
+
+describe('gate', () => {
+
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.Provider.env());
+
+  const program = anchor.workspace.Gate as Program<Gate>;
+
+  it('Is initialized!', async () => {
+    // Add your test here.
+    const tx = await program.rpc.initialize({});
+    console.log("Your transaction signature", tx);
+  });
+});
