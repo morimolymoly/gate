d333054c43fa865cf68545eeee22cb037963cbd9
morimolymoly <morimolymoly@gmail.com>
2021-11-15 12:14:24 +0900

    fetch taiga nft

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,20 +1,75 @@
 import './App.css';
-import { WalletNotConnectedError } from '@solana/wallet-adapter-base';
-import { useConnection, useWallet } from '@solana/wallet-adapter-react';
-import { Keypair, SystemProgram, Transaction } from '@solana/web3.js';
-import React, { FC, useCallback } from 'react';
-import { Wallet } from './wallet'
+import {
+  Program, Provider, web3
+} from '@project-serum/anchor';
 
+import { Connection, programs } from '@metaplex/js';
+import { WalletNotConnectedError } from '@solana/wallet-adapter-base';
+import { getPhantomWallet } from '@solana/wallet-adapter-wallets';
+import { useWallet, WalletProvider, ConnectionProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+require('@solana/wallet-adapter-react-ui/styles.css');
+
+const wallets = [
+  /* view list of available wallets at https://github.com/solana-labs/wallet-adapter#wallets */
+  getPhantomWallet()
+];
+
+const connection = new Connection('devnet');
 
 function App(): JSX.Element {
-  const { connection } = useConnection();
-  const { publicKey, sendTransaction } = useWallet();
+  const wallet = useWallet();
+
+  console.log(wallet);
+
+  async function onClick() {
+    if (!wallet.publicKey) throw new WalletNotConnectedError();
+
+    const con = new web3.Connection("https://api.devnet.solana.com");
+    const filter: web3.TokenAccountsFilter =  {
+      programId: new web3.PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA")
+    };
+    const test = await con.getTokenAccountsByOwner(wallet.publicKey, filter);
+    console.log(test.value[0].pubkey);
+    const tokenAddress = test.value[0].pubkey;
+    console.log(tokenAddress.toString());
+
+    const run = async () => {
+      try {
+        const ownedMetadata = await programs.metadata.Metadata.load(connection, "8Yvyrz7AosXNyt2xocpbJdmDEqpM73pme66uoGG2rkwM");
+        console.log(ownedMetadata);
+      } catch {
+        console.log('Failed to fetch metadata');
+      }
+    };
+    run();
+  }
   
-  return (
-    <div className="App">
-      <Wallet></Wallet>
-    </div>
-  );
+  if (wallet.connected) {
+    return (
+      <div className="App">
+        <button onClick={onClick}>
+              check tokens
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <div className='app'>
+        <WalletMultiButton />
+      </div>
+    );
+  }
 }
 
-export default App;
+const AppWithProvider = () => (
+  <ConnectionProvider endpoint="https://api.devnet.solana.com">
+    <WalletProvider wallets={wallets} autoConnect>
+      <WalletModalProvider>
+        <App />
+      </WalletModalProvider>
+    </WalletProvider>
+  </ConnectionProvider>
+)
+
+export default AppWithProvider;
